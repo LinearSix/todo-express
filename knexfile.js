c72dcaa3f23499ca7d55237f5c29fa5a9076bec2
Brent Gardner <bgardner@squarelabs.net>
2017-03-28 21:47:32 -0500

    hack ssl

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,8 @@
+// HACK: to work around heroku standard plans requiring SSL but not specifying in connection string
+if(process.env.DATABASE_URL) {
+  var pg = require('pg');
+  pg.defaults.ssl = true;
+}
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL || { 
